ab97c2fbd661d29229d17923a5c6197a130456a3
delo-sys <mrodhiambo226@gmail.com>
2025-02-25 16:34:19 +0300

    end of introducton

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+// This tell the server that php code begins here
+// Eerything between <?php and ? > is processed as php code
+
+//echo is language construct that output text/HTML
+
+echo "Hello,world!";
+
+// this marks the end of php code 
+?>
---
+++ intro.php
@@ -0,0 +1,12 @@
+introducton to PHP
+what is PHP 
+PHP (hypertext processor) is a server side (language executed by server) scripting language desinged specifically web development.
+To understand how websites work
+
+1:When you visit a websites,w browser(the client)sennd a request to web server 
+the server processes this request.
+For static website using php,the server 
+*Executes the php 
+* generate HTML , based on the codes output 
+*send the HTMl back to the browser.
+example Think jof like ordering at a resturant you (browser) make an order (request) the (php) prepares you need (process the code) the waiter (server) brings you the finished dish (HTML)
